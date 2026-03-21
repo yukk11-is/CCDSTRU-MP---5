87bb7c371d78b2ab6643c26885e90b74fe1f8544
Jay Aberin <shanjay_aberin@dlsu.edu.ph>
2026-03-21 15:11:08 +0800

    Implemented Remove function

--- CCDSTRU_MP(INTERPRETATIONS).c
+++ CCDSTRU_MP(INTERPRETATIONS).c
@@ -9,6 +9,19 @@ struct coordTag{ //stores x and y coordinates as structs for easier record keepi
 };
 
 typedef struct{
+	
+	/*
+	NOTE: the arrays below do not hold arrays, they hold boolean (0,1) that says if 
+	coordinate belongs to the set. ALSO we will not be following the row-major attribute
+	of the C language, so that the array indices and cartesian coordinates.
+	^^^^^^^^^^^^^^^^^^^^^^^^^^PLEASE TAKE NOTE OF THIS^^^^^^^^^^^^^^^^^^^^^^^^^^
+	
+	Example:
+	Is coordinate (2,3) found in set B?
+	B[2][3] = 0; 2 is the x coord,
+	
+	*/
+	
 	//R,B,S,T are all initialized as NULL
 	int R [4][4]; //Coordinates owned by R
 	int B [4][4]; //Coordinates owned by B
@@ -29,13 +42,29 @@ typedef struct{
     
 }gameState;
 
+//Function prototypes
+int checkValid(coord pos);
+
 /*
 NOTE: I have initially set all function types to void, 
 this may change depending on the requirements later on
 */
 
-void Remove (coord *pos){
-	
+void Remove (gameState *status, coord pos){
+if(checkValid(pos)){
+	if(status->go==1){ //Remove pos from set R
+		status->R[pos.x][pos.y] = 0;
+			
+	}else{// Remove pos from set B
+		status->B[pos.x][pos.y] = 0;
+	}
+		
+	//Remove pos from S
+	status->S[pos.x][pos.y] = 0;
+		
+	//Remove pos from T
+	status->T[pos.x][pos.y] = 0;
+}
 }
 
 void Replace(coord *pos){
@@ -70,12 +99,20 @@ int checkGame(gameState status){
 	return 1;
 }
 
+//initializes all array elements to 0 (NULL)
 void ZeroArray (int array[][4]){
 	int i,j;
 	for(i=0;i<4;i++){
 		for(j=0;j<4;j++)
 			array[i][j] = 0;
 	}
+}
+
+//checks if the coord is valid returns 1 if yes, 0 otherwise;
+int checkValid(coord pos){
+	if((pos.x>0&&pos.x<4)&&(pos.y>0&&pos.y<4))
+		return 1;
+	return 0;	
 }
 
 
